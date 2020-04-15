441445eb2e66e8e2387bd90d6c2f133876919dbd
Jose <jose.carreres.81@gmail.com>
2020-04-15 02:43:46 +0200

    Ruta users.js, endpoint introducir usuario

--- routes/users.js
+++ routes/users.js
@@ -19,9 +19,38 @@ router.get('/:id', async(req, res) => {
     const user = await User.findById(req.params.id)
     if(!user) return res.status(404).send('No hemos encontrado un usuario con ese ID')
     res.send(user)
-  })
-  //#endregion
+})
+//#endregion
+
+
+//#region Introducir user
+router.post('/', async (req, res)=> {
   
+    //Analiza los resultados de la validación del request
+    const errors = validationResult(req);
+    //Si error llega distinto que vacio es que a encontrado algun error
+    if (!errors.isEmpty()) {
+      //Devuelve un 422 y en formato json el error
+      return res.status(422).json({ errors: errors.array() });
+    }
+    
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      address: req.body.address,
+      country: req.body.country,
+      province: req.body.province,
+      zip: req.body.zip
+    })
+  
+    // Guarda el user
+    const result = await user.save()
+    
+    res.status(201).send(result)
+})
+//#endregion
+
 
 
 module.exports = router;
