b0a9066e9185013e75422667b94886a03e5bda1a
Jose <jose.carreres.81@gmail.com>
2020-04-16 04:43:26 +0200

    Route movies.js, endpoint filtrar pelicula por titulo

--- routes/movies.js
+++ routes/movies.js
@@ -27,6 +27,15 @@ router.get('/:id', async(req, res) => {
 
 
 
+//#region Listar pelicula por titulo
+router.get('/title/:title', async(req, res)=> {
+  const movies = await Movie.find({title: req.params.title})
+  res.send(movies)
+}) 
+//#endregion
+
+
+
 //#region Introducir pelicula, datos Embebido
 router.post('/', async (req, res)=> {
 
