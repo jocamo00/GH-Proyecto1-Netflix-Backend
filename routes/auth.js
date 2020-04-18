6ca57e777e4400f5fcd8ee07a07a8a7ee1cdd62f
Jose <jose.carreres.81@gmail.com>
2020-04-18 03:18:08 +0200

    Ruta auth.js, endpoint de login

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const express = require('express');
+const { User } = require('../models/user')
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+
+
+//#region Introducir usuario
+router.post('/', async (req, res)=> {
+
+    // Recoje el email y comprueba si existe o no
+    let user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('Usuarios o contraseña incorrectos')
+
+
+    // Se compara el password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).send('Usuario o contraseña incorrectos')
+
+
+    res.send('Usuario y contraseña corercta')
+
+})
+
+
+module.exports = router
