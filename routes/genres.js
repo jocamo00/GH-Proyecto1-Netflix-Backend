eaa0096f58c511520b15e26ddda51cd714ede440
Jose <jose.carreres.81@gmail.com>
2020-04-18 20:36:28 +0200

    Roles en ruta genres.js

--- routes/genres.js
+++ routes/genres.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const express = require('express');
+const auth = require('../middleware/auth')
+const Role = require('../helpers/role')
+const authorize = require('../middleware/role')
 const { Genre } = require('../models/genre')
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
 
 //#region Listar todos los generos
-router.get('/', async(req, res)=> {
+router.get('/', [auth, authorize([Role.Admin])], async(req, res)=> {
   const genres = await Genre.find()
   res.send(genres)
 }) 
@@ -15,7 +18,7 @@ router.get('/', async(req, res)=> {
 
 
 //#region  Listar género por id
-router.get('/:id', async(req, res) => {
+router.get('/:id', [auth, authorize([Role.Admin])], async(req, res) => {
     // recoje el id de la url
     const genre = await Genre.findById(req.params.id)
     if(!genre) return res.status(404).send('No hemos encontrado un género con ese ID')
@@ -26,7 +29,7 @@ router.get('/:id', async(req, res) => {
 
 
 //#region Introducir género
-router.post('/', async (req, res)=> {
+router.post('/', [auth, authorize([Role.Admin])], async (req, res)=> {
     
     const genre = new Genre({
       name: req.body.name
@@ -41,7 +44,7 @@ router.post('/', async (req, res)=> {
 
 
 //#region Editar el género seleccionado por id  
-router.put('/:id', async (req, res)=> {
+router.put('/:id', [auth, authorize([Role.Admin])], async (req, res)=> {
   
     const genre = await Genre.findByIdAndUpdate(req.params.id, {
         name: req.body.name
@@ -63,7 +66,7 @@ router.put('/:id', async (req, res)=> {
 
 
 //#region Eliminar género por id  
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', [auth, authorize([Role.Admin])], async (req, res) => {
 
     const genre = await Genre.findByIdAndDelete(req.params.id)
     
